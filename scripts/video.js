6d73cac120c5f3bad4867017bd0d0de3dd0403f6
shad-bin-abi-aydid <shadaydid@gmail.com>
2025-02-05 21:07:10 +0000

    Videos data are loaded from API and start displaying in UI

--- scripts/video.js
+++ scripts/video.js
@@ -1,18 +1,87 @@
 // Fetch, Load and show categories on html
 
 // Create LoadCategories
-const LoadCategories = () =>{
-    // fetch the data
-    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+const loadCategories = () => {
+  // fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayData(data.categories))
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 };
 
+// Create LoadVideos
+const loadVideos = () => {
+  // fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.videos))
+    .catch((error) => console.log(error));
+};
 
 // Create DisplayCategories
-const displayData = (data) =>{
-    console.log(data);
+const displayData = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+
+  categories.forEach((item) => {
+    // create button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    // add button to category container
+    categoryContainer.append(button);
+  });
+};
+
+// Object demo from API. it add here to see and write codes.
+
+const demoCode = {
+    "category_id": "1001",
+    "video_id": "aaab",
+    "thumbnail": "https://i.ibb.co/QPNzYVy/moonlight.jpg",
+    "title": "Midnight Serenade",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/fDbPv7h/Noha.jpg",
+            "profile_name": "Noah Walker",
+            "verified": false
+        }
+    ],
+    "others": {
+        "views": "543K",
+        "posted_date": ""
+    },
+    "description": "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
 }
 
-LoadCategories();
+
+
+
+// Create DisplayVideos
+const displayVideo = (videos) => {
+    console.log(videos);
+  const videoContainer = document.getElementById("videos");
+
+  videos.forEach((video) => {
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+         <figure>
+            <img
+            src="${video.thumbnail}"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+    videoContainer.append(card);
+  });
+};
+
+loadCategories();
+loadVideos();
