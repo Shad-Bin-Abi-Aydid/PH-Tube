8dcc15b465a31a573cfc2fe7e76ae431cf1235bf
shad-bin-abi-aydid <shadaydid@gmail.com>
2025-02-07 21:13:48 +0000

    All button functionable now and videos are coming category wise now.

--- scripts/video.js
+++ scripts/video.js
@@ -1,3 +1,15 @@
+
+// Date formating (Hours, minutes, second) and Showing
+
+function getTimeString(time){
+  const hours = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  const minute = parseInt( remainingSecond / 60);
+  remainingSecond = minute % 60;
+
+  return `${hours}hr ${minute}min ${remainingSecond}sc ago`;s
+}
+
 // Fetch, Load and show categories on html
 
 // Create LoadCategories
@@ -18,18 +30,32 @@ const loadVideos = () => {
     .catch((error) => console.log(error));
 };
 
+// Load Category videos 
+const loadCategoryVideos = (id) =>{
+  // fetch the data
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.category))
+    .catch((error) => console.log(error));
+}
+
 // Create DisplayCategories
 const displayData = (categories) => {
   const categoryContainer = document.getElementById("categories");
 
   categories.forEach((item) => {
     // create button
-    const button = document.createElement("button");
-    button.classList = "btn";
-    button.innerText = item.category;
+    console.log(item.category_id);
+    const buttonContainer = document.createElement("div");
 
+    buttonContainer.innerHTML = 
+    `
+      <button onClick = "loadCategoryVideos(${item.category_id})" class="btn">${item.category} </button>
+    `;
+    
+    
     // add button to category container
-    categoryContainer.append(button);
+    categoryContainer.append(buttonContainer);
   });
 };
 
@@ -58,29 +84,63 @@ const displayData = (categories) => {
 
 // Create DisplayVideos
 const displayVideo = (videos) => {
-    console.log(videos);
+ // console.log(videos);
+
   const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML = "";
+
+  // check data is available or not 
+  if(videos.length == 0){
+    // remove the grid from classList to show the html in middle
+    videoContainer.classList.remove("grid");
+
+    // added the innerHTML if there have no content
+    videoContainer.innerHTML = `
+      <div class = "min-h-[300px] flex flex-col justify-center items-center gap-5">
+        <img src = "assets/Icon.png" />
+        <h2 class = "font-bold text-2xl">Oops!! Sorry, There is no content here</h2>
+      </div>
+    `
+    return;
+  }
+  else{
+    // added the grid again otherwise all categories videos showing column by column
+    videoContainer.classList.add("grid");
+  }
 
   videos.forEach((video) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-         <figure class = "h-[200px]">
+         <figure class = "h-[200px] relative">
             <img
             src="${video.thumbnail}"
             class = "h-full w-full object-cover"
             alt="Shoes" />
+            ${
+              video.others.posted_date?.length == 0
+                ? ""
+                : `<span class = "absolute right-2 bottom-2 bg-black text-white p-2 rounded-lg text-xs"> ${getTimeString(video.others.posted_date)}</span>`
+            }
+            
         </figure>
         <div class="px-0 py-2 flex gap-2">
             <div>
-              <img class = "h-10 w-10 rounded-full object-cover" src =${video.authors[0].profile_picture} />
+              <img class = "h-10 w-10 rounded-full object-cover" src =${
+                video.authors[0].profile_picture
+              } />
             </div>
 
             <div>
               <h2 class = "font-bold">${video.title}</h2>
               <div class = "flex gap-2 items-center">
                 <p class = "text-gray-500">${video.authors[0].profile_name}</p>
-                <img class = "h-5 w-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" />
+                ${
+                  video.authors[0].verified === true
+                    ? '<img class = "h-5 w-5" src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000" />'
+                    : ""
+                }
+                
               </div>
               <p class = "text-gray-500">${video.others.views} Views</p>
             </div>
